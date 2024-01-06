23353a1f9e2e66ac4555a5fdee994eb98ba92b76
Cleberson Vieira <klbrson@gmail.com>
2024-01-06 12:19:06 -0300

    feat(Colaborador): Excluindo coloboradora

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -6,7 +6,7 @@ const Time = ({time, colaboradores, aoDeletar, mudarCor}) => {
     return (
 
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, 0.3) }}>
-            <input type='color' value={time.cor} onChange={evento => mudarCor(evento.target.value, time.nome)} className='input-cor' />
+            <input type='color' value={time.cor} onChange={evento => mudarCor(evento.target.value, time.id)} className='input-cor' />
             <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar} />)}
